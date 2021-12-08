eded6672c89172a8775e90d64c141ab21024772e
Jianjun Lv <jianjlv@microsoft.com>
2021-12-08 20:20:56 +0800

    Test commit

--- src/test/java/ARun.java
+++ src/test/java/ARun.java
@@ -11,6 +11,6 @@ import java.util.*;
 public class ARun {
     public static void main(String[] args)
     {
-        System.out.println(1234);
+        System.out.println("Main test method run.");
     }
 }
